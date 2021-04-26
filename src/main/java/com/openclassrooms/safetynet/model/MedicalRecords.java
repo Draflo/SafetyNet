8589a8450ab055a388d6896a19f751dbf1ac1b56
Florent TRIMOREAU <flo2533@hotmail.fr>
2021-04-26 18:26:36 +0200

    Trying converter for database

--- src/main/java/com/openclassrooms/safetynet/model/MedicalRecords.java
+++ src/main/java/com/openclassrooms/safetynet/model/MedicalRecords.java
@@ -2,29 +2,49 @@ package com.openclassrooms.safetynet.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.openclassrooms.safetynet.model.converter.ListStringConverter;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Entity
+@AllArgsConstructor
 @Table(name = "medicalrecords")
 public class MedicalRecords {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+	
+	@Column(name = "first_name")
 	private String firstname;
+	
+	@Column(name = "last_name")
 	private String lastname;
+	
+	@Column(name = "birthdate")
 	private Integer birthdate;
 	
+	@Column(name = "medications")
+	@Convert(converter = ListStringConverter.class)
 	private List<String> medications;
+	
+	@Column(name = "allergies")
+	@Convert(converter = ListStringConverter.class)
 	private List<String> allergies;
 	
-	public MedicalRecords () {}
+
+	public MedicalRecords() {
+	}
 
 }
---
+++ src/main/java/com/openclassrooms/safetynet/model/converter/ListStringConverter.java
@@ -0,0 +1,25 @@
+package com.openclassrooms.safetynet.model.converter;
+
+import java.util.List;
+
+import javax.persistence.AttributeConverter;
+
+public class ListStringConverter implements AttributeConverter<List<String>, String> {
+	
+	private static final String SEPARATOR = "; ";
+
+	@Override
+	public String convertToDatabaseColumn(List<String> list) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(list).append(SEPARATOR);
+		return sb.toString();
+	}
+
+	@Override
+	public List<String> convertToEntityAttribute(String dbData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
